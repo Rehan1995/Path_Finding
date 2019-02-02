00e11c10935405397694da4d6cce7051baa8f21d
Rehan samarasekera <sewranga77@gmail.com>
2019-02-02 19:01:20 +0530

    change variables

--- src/NodeClass.java
+++ src/NodeClass.java
@@ -2,16 +2,16 @@
 * Created by Rehan Samarasekera on 4/2/2017. [2015065] [w1583026]
  */
 class NodeClass {
-        int len_X;
-        int len_Y;
+        int len_Xxx;
+        int len_Yyy;
         double distance = Integer.MAX_VALUE;
         NodeClass parent = null;
         boolean visited;
         boolean blocked;
 
         public NodeClass(int x, int y) {
-            this.len_X= x;
-            this.len_Y = y;
+            this.len_Xxx= x;
+            this.len_Yyy = y;
         }
        
     }
